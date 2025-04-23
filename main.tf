3a04472e6286541dc6e3103614890df5fb06795d
chisan ahn <chisan2001@gmail.com>
2025-04-23 10:34:14 +0900

    chore: 폴더구조 단순화
    현재는 개발/운영 환경을 분리할 계획 없음

--- main.tf
+++ main.tf
@@ -30,19 +30,20 @@ data "http" "myip" {
 
 locals {
   my_ip = "${data.http.myip.response_body}/32"
+  project_name = "annyang"  # 프로젝트 이름 변경
 }
 
 # SSM Parameter Store에서 RDS 비밀번호 가져오기
 # 이 파라미터는 이미 생성되어 있어야 함 - AWS 콘솔에서 생성 가능
 data "aws_ssm_parameter" "db_password" {
-  name = "/dev/db/password"
+  name = "/db/password"  # 경로 변경
   # 파라미터가 없는 경우 자동으로 생성
   depends_on = [aws_ssm_parameter.db_password]
 }
 
 # SSM Parameter Store에 RDS 비밀번호 생성 (없는 경우)
 resource "aws_ssm_parameter" "db_password" {
-  name  = "/dev/db/password"
+  name  = "/db/password"  # 경로 변경
   type  = "SecureString"
   value = "Password123!"  # 초기값 설정 - 나중에 AWS 콘솔에서 변경 가능
   
@@ -53,24 +54,24 @@ resource "aws_ssm_parameter" "db_password" {
 }
 
 module "vpc" {
-  source = "../../modules/vpc"
+  source = "./modules/vpc"
 
-  project_name = "dev"
+  project_name = local.project_name
 }
 
 # 보안 그룹 모듈 추가
 module "sg" {
-  source = "../../modules/sg"
+  source = "./modules/sg"
 
-  project_name = "dev"
+  project_name = local.project_name
   vpc_id       = module.vpc.vpc_id
   admin_ip     = local.my_ip  # 현재 IP 주소 전달
 }
 
 module "rds" {
-  source = "../../modules/rds"
+  source = "./modules/rds"
 
-  project_name = "dev"
+  project_name = local.project_name
   subnet_ids   = module.vpc.private_subnet_ids
   
   # 보안 그룹 모듈에서 생성된 ID 사용
@@ -90,11 +91,11 @@ module "rds" {
 }
 
 module "ec2" {
-  source = "../../modules/ec2"
+  source = "./modules/ec2"
 
-  project_name = "dev"
+  project_name = local.project_name
   subnet_id    = module.vpc.public_subnet_ids[0]
-  associate_public_ip     = true
+  associate_public_ip = true
   
   # 보안 그룹 모듈에서 생성된 ID 사용
   security_group_id = module.sg.ec2_security_group_id
@@ -114,4 +115,4 @@ output "rds_endpoint" {
 output "mysql_connection_command" {
   description = "MariaDB 접속 명령어"
   value       = "mysql -h ${module.rds.db_instance_address} -P ${module.rds.db_instance_port} -u ${module.rds.db_instance_username} -p"
-}
+} 
