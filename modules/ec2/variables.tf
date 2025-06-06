116364a828a541ac3a9c49256769157a1202efa7
chisan ahn <chisan2001@gmail.com>
2025-06-07 01:48:31 +0900

    feat: EC2 인스턴스 스펙업

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -17,7 +17,7 @@ variable "ami_id" {
 variable "instance_type" {
   description = "EC2 인스턴스 타입"
   type        = string
-  default     = "t2.micro"
+  default     = "t2.medium"
 }
 
 variable "associate_public_ip" {
